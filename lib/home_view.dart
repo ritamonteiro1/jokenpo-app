2517ae5f040d843188143fc3ea6b50673b9ad5ea
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2021-08-19 10:53:33 -0300

    create function _alternativeOption in _HomeViewState

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class HomeView extends StatefulWidget {
   @override
@@ -6,6 +7,50 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  var _appImage = AssetImage('assets/images/pattern.png');
+  var _messagem = 'Escolha uma opção abaixo!';
+
+  _alternativeOption(String clickedImage) {
+    var _options = ['rock', 'paper', 'scissors'];
+    var _number = Random().nextInt(3);
+    var _appChoice = _options[_number];
+    switch (_appChoice) {
+      case 'rock':
+        setState(() {
+          this._appImage = AssetImage('assets/images/rock.png');
+        });
+        break;
+      case 'paper':
+        setState(() {
+          this._appImage = AssetImage('assets/images/paper.png');
+        });
+        break;
+      case 'scissors':
+        setState(() {
+          this._appImage = AssetImage('assets/images/scissors.png');
+        });
+        break;
+    }
+
+    if (clickedImage == 'rock' && _appChoice == 'scissors' ||
+        clickedImage == 'scissors' && _appChoice == 'paper' ||
+        clickedImage == 'paper' && _appChoice == 'rock') {
+      setState(() {
+        _messagem = 'Você ganhou!';
+      });
+    } else if (_appChoice == 'rock' && clickedImage == 'scissors' ||
+        _appChoice == 'scissors' && clickedImage == 'paper' ||
+        _appChoice == 'paper' && clickedImage == 'rock') {
+      setState(() {
+        _messagem = 'Você perdeu!';
+      });
+    } else {
+      setState(() {
+        _messagem = 'Empate!';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,20 +62,51 @@ class _HomeViewState extends State<HomeView> {
         children: [
           Padding(
             padding: const EdgeInsets.only(top: 32, bottom: 16),
-            child: Text('Escolha do App:', textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,),),
+            child: Text(
+              'Escolha do App:',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-          Image.asset('assets/images/pattern.png'),
+          Image(
+            image: _appImage,
+          ),
           Padding(
             padding: const EdgeInsets.only(top: 32, bottom: 16),
-            child: Text('Você perdeu!', textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,),),
+            child: Text(
+              this._messagem,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-            Image.asset('assets/images/rock.png', height: 100,),
-            Image.asset('assets/images/paper.png', height: 100,),
-            Image.asset('assets/images/scissors.png', height: 100,)
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            GestureDetector(
+              onTap: () => _alternativeOption('rock'),
+              child: Image.asset(
+                'assets/images/rock.png',
+                height: 100,
+              ),
+            ),
+            GestureDetector(
+              onTap: () => _alternativeOption('paper'),
+              child: Image.asset(
+                'assets/images/paper.png',
+                height: 100,
+              ),
+            ),
+            GestureDetector(
+              onTap: () => _alternativeOption('scissors'),
+              child: Image.asset(
+                'assets/images/scissors.png',
+                height: 100,
+              ),
+            ),
           ]),
         ],
       ),
